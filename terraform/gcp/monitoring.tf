b674d8d78ce88c16cf4a9e581d3a51cadf6db14a
unknown <jaredtalon88@gmail.com>
2026-08-25 09:17:28 +0900

    continued fight with provider

--- terraform/gcp/monitoring.tf
+++ terraform/gcp/monitoring.tf
@@ -18,16 +18,39 @@ resource "google_monitoring_service" "miniflux" {
   }
 }
 
+# Ongoing provider issues with SLOs. I attempted an import of an SLO created in
+# the UI, however it's state does not contain a "service" field, which is 
+# required for this terraform resource. 
+
+# resource "google_monitoring_slo" "request_based_slo" {
+#     calendar_period     = "WEEK"
+#     deletion_policy     = "DELETE"
+#     display_name        = "99% - Availability - Calendar week"
+#     goal                = 0.99
+#     id                  = "projects/80024592637/services/dBXFKZf0QCSKlkxzNxyw5g/serviceLevelObjectives/GHSBuJOuRHW5GOJHQSy4jA"
+#     name                = "projects/80024592637/services/dBXFKZf0QCSKlkxzNxyw5g/serviceLevelObjectives/GHSBuJOuRHW5GOJHQSy4jA"
+#     project             = "bishop-sre-example"
+#     rolling_period_days = 0
+#     slo_id              = "GHSBuJOuRHW5GOJHQSy4jA"
+#     user_labels         = {}
+
+#     basic_sli {
+#         location = []
+#         method   = []
+#         version  = []
+
+#         availability {
+#             enabled = true
+#         }
+#     }
+# }
+
 resource "google_monitoring_slo" "request_based_slo" {
-  service         = google_monitoring_service.miniflux.id
+  service         = google_monitoring_service.miniflux.service_id
+  goal            = 0.99
   calendar_period = "WEEK"
-  display_name    = "99% - Availability - Calendar week"
-
-  goal = 0.99
-
   basic_sli {
     availability {
-
     }
   }
 }
